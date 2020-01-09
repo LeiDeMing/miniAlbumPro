3bca5c38e005597d7c61903d9ec573db004f437d
LeiDeMing <136371773@qq.com>
2020-01-09 17:48:20 +0800

    add

---
+++ service/lib/db/model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    openId: {
+        type: String,
+        index: true,
+        unique: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    lastLogin: {
+        type: Date
+    },
+    name: {
+        type: String,
+        index: true
+    },
+    avatar: {
+        type: String
+    },
+    userType: {
+        type: Number
+    }
+})
