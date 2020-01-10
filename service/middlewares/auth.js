628d1f55493d20310cfc3a799f86ff139cd0b7d5
LeiDeMing <136371773@qq.com>
2020-01-10 22:12:14 +0800

    add

--- service/middlewares/auth.js
+++ service/middlewares/auth.js
@@ -2,4 +2,8 @@ module.exports = async function (ctx, next) {
     const sessionKey = ctx.get('x-session');
     if(!sessionKey) ctx.throw(401,'请求头中未包含x-session');
     const user = await findBySessionKey(sessionKey);
+
+    if(/^\/admin/i.test(ctx.url) && !ctx.state.user.isAdmin){
+        ctx.throw(401,'当前资源仅支持管理员访问');
+    }
 }
